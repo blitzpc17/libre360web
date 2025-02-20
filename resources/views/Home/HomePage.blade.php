8136eb8cbdcf162dbdfae0bb09046fa60fa575e1
blitzpc17 <remizeroaw117@gmail.com>
2025-02-20 00:17:23 -0600

    hero

--- resources/views/Home/HomePage.blade.php
+++ resources/views/Home/HomePage.blade.php
@@ -6,25 +6,32 @@
 @section('contenido')
     <main>
         <section class="hero">
-            <h1>Viaja seguro, viaja <span class="highlight">Libre 360°</span></h1>
+            <h2>Viaja seguro, viaja <span class="highlight">Libre 360°</span></h2>
             <p>Rápido, cómodo y seguro.</p>
-            <div class="hero-content">
-                <div class="mapa">
-                    <p>Cotiza tu viaje <strong>$80.47</strong></p>
+
+            <div class="row">
+
+                <div class="option">
+                    <img src="{{asset('Home/img/1080X1080-CHOFER.png')}}" alt="conductor">
+                    <a class="btn-libre360">Viaja</a>
                 </div>
-                <div class="imagen-manejo"></div>
+
+                <div class="option">
+                    <img src="{{asset('Home/img/1080X1080-CHOFER.png')}}" alt="conductor">
+                    <a class="btn-libre360">Comienza a manejar</a>
+                </div>
+                
             </div>
-            <button class="btn-primary">Viaja</button>
-            <button class="btn-secondary">Comienza a manejar</button>
+      
         </section>
 
         <section class="promo">
             <h2>¡Acumula puntos y viaja gratis!</h2>
         </section>
 
-        <section class="info">
-            <h2>Estamos aquí, para llevarte a donde necesites</h2>
-            <p>Somos un servicio de transporte público comprometido con ofrecerte comodidad, rapidez y seguridad.</p>
+        <section class="info">            
+            <p>Somos un servicio de transporte público comprometido con ofrecerte comodidad, rapidez y seguridad en cada trayecto. Nuestra misión es llevarte a tu destino de forma eficiente, asegurándonos de que tu experiencia sea siempre agradable y confiable.</p>
+            <h2>¡Estamos aquí, para llevarte a donde necesites!</h2>
         </section>
     </main>
 @endsection
