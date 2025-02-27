532e95d24eca79962e0d3b8edb2cfc6c102e711e
blitzpc17 <remizeroaw117@gmail.com>
2025-02-27 16:39:59 -0600

    complemento de la seccion de registro.

--- resources/views/Home/layout.blade.php
+++ resources/views/Home/layout.blade.php
@@ -6,7 +6,7 @@
     <title>Libre360 - @yield('title')</title>
 
     @stack('css')
-    <link rel="stylesheet" href="{{asset('Home/css/prueba1.css')}}">
+    <link rel="stylesheet" href="{{asset('Home/css/prueba2.css')}}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
 </head>
